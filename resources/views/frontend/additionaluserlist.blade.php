1154b3277f345410f07564211f0cff99f5b66fa4
loopcon <loopconinfo@gmail.com>
2024-02-21 16:50:58 +0530

    changes done

--- resources/views/frontend/additionaluserlist.blade.php
+++ resources/views/frontend/additionaluserlist.blade.php
@@ -41,11 +41,15 @@
                                         <div class="col-md-12 text-right">
                                             <div class="col-md-12 text-right">
 
-                                            <a href="javascript:void(0)"  class="btn text-light freetoproacess"  style="background:#4099ff">  Free to Pro</a>
-                                              @if($customer->access_type!="paid"  && $customer->access_type!="additionaluser"  && $customer->access_type!="requestforadditional" && $customer->payment!=1)
-								                
-								                @endif
+                                           
 
+                                             <!-- freetopro -->
+                                              @if($customer->access_type!="paid"  && $customer->access_type!="additionaluser"  && $customer->access_type!="requestforadditional" && $customer->payment!=1)
+								                 <a href="javascript:void(0)"  class="btn text-light freetoproacess"  style="background:#4099ff">  Free to Pro</a>
+								                @endif
+								                <!-- endfreetopro -->
+
+								                <!-- paid -->
 								                @if($customer->remain_payment_additional_user > 0)
 								                <a href="javascript:void(0)"  class=" btn text-light additionaluser"  style="background:#4099ff">Add Additional User</a>
 								                @endif
@@ -92,26 +96,299 @@
         <div id="styleSelector">
         </div>
     </div>
+
+
+    <!-- Free to Pro Access-->
+<div class="modal fade" id="freetopromodel" tabindex="-1" role="dialog" aria-labelledby="scoreImportModalLabel" aria-hidden="true">
+    <div class="modal-dialog  modal-dialog-centered modal-lg">
+        <div class="modal-content">
+
+            <div class="modal-header">
+              <h5 class="modal-title" id="sizeOptionModalLabel">Free to Pro</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            </div>
+            <div class="modal-body">
+             <div class="row m-0">
+                    <div class="col-12 col-md-12 p-0">
+                        <div class="login-register-form-box">
+                            <div>
+                                <form method="post" id="freetoproform">
+                                    @csrf
+                                    <input type="hidden" name="id" value="{{$customer->id}}">
+                                    <div class="row mb-3">
+                                        <div class="col-12 col-md-4 ">
+                                            <div class="input-group">
+                                                <input type="text" id="name" name="name" class="form-control" placeholder="Name" value="{{$customer->name}}">
+                                            </div>
+                                            <div id="nameerror"></div>
+                                        </div>
+
+                                        <div class="col-12 col-md-4">
+                                            <div class="input-group">
+                                                <input type="email" id="email" name="email" class="form-control" placeholder="Email" value="{{$customer->email}}" readonly> 
+                                            </div>
+                                            <div id="emailerror"></div>
+                                        </div> 
+
+                                        <div class="col-12 col-md-4" >
+                                            <div class="input-group">
+                                                <input  class="form-control" maxlength="10"  type="text" oninput="this.value=this.value.replace(/[^0-9]/g,'');" placeholder="Phone Number" id="phone" name="phone" value="{{$customer->phone}}" readonly>
+                                            </div> 
+                                            <div id="phoneerror"></div>  
+                                        </div>
+
+                                    </div>
+
+                                    <div class="row mb-3">
+                                    
+                                        <div class="col-12 col-md-6">
+                                            <div class="input-group ">
+                                                <input type="text" id="job_title" name="job_title"  class="form-control" placeholder="Job Title" value="{{$customer->job_title}}">
+                                            </div>
+                                            <div id="jobtitleerror"></div>
+                                        </div>
+                                     
+                                        <div class="col-12 col-md-6">
+                                            <div class="input-group">
+                                                <input type="text" id="bussiness_name" name="bussiness_name" class="form-control" placeholder="Business Name" value="{{$customer->bussiness_name}}"> 
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="row mb-3">
+                                        <div class="col-12 col-md-12">
+                                            <div class="input-group">
+                                                <input class="form-control" placeholder="If Your Business is Part of a Wider Group Please let us  Know" class="form-control" name="business_wider_group" value="{{$customer->business_wider_group}}">
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="row mb-3">
+                                        <div class="col-12 col-md-12">
+                                            <div class="input-group">
+                                                <textarea class="form-control" placeholder="Biling Address" class="form-control" name="billing_address" placeholder="Biling Address">{{$customer->billing_address}}</textarea> 
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="row mb-3">
+                                           <div class="col-12 col-md-12">
+                                            <div class="input-group ">
+                                                <input type="text" id="additional_user_no" name="additional_user_no"  class="form-control" placeholder="Number of Additional User" value="" oninput="this.value=this.value.replace(/[^0-9]/g,'');">
+                                            </div>
+                                            <div id="jobtitleerror"></div>
+                                        </div>
+                                    </div>
+
+                                    <div class="row mb-3">
+                                        <div class="col-12 col-md-12">
+                                            <div class="input-group">
+                                                <textarea class="form-control"class="form-control" name="additional_details" placeholder="Is there is Anything You Require Additional on Your Invoice"></textarea> 
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div id="errormsg"></div>
+                                    <button type="button" id="close-modal" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                        <button type="button" class="btn btn-primary" id="save_freetopro">Submit</button>
+                                    </div>
+                                </form>
+                            </div>
+
+                        </div>
+                    </div>
+              </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<!-- End Free to Pro Access-->
+<div class="modal fade" id="additionalusermodel" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog  modal-dialog-centered modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="sizeOptionModalLabel">Additional User</h5>
+                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            </div>
+            <div class="modal-body" style="max-height:400px;overflow: auto;">
+             <div class="row m-0">
+                    <div class="col-12 col-md-12 p-0">
+                        <div class="login-register-form-box">
+                            <div>
+                                @if($customer->remain_payment_additional_user > 0)
+                            <form method="post" id="additionaluserform">
+                                @csrf
+                                <input type="hidden" name="id" value="{{$customer->id}}">
+                                <?php $j=1;?>
+                              @for($i = 0; $i < $customer->remain_payment_additional_user; $i++)
+                                <label>Add Details for Additional User {{$j}}</label>
+                                <?php $j++;?>
+                                <div class="row mb-3">
+                                    <div class="col-12 col-md-6 ">
+                                        <div class="input-group">
+                                            <input type="text" id="name{{$i}}" name="name[]" class="form-control" placeholder="Name">
+                                        </div>
+                                        <div id="nameerror{{$i}}"></div>
+                                    </div>
+
+                                    <div class="col-12 col-md-6">
+                                        <div class="input-group ">
+                                            <input type="text" id="job_title{{$i}}" name="job_title[]" class="form-control" placeholder="Job Title">
+                                        </div>
+                                        <div id="jobtitleerror{{$i}}"></div>
+                                    </div>
+                                </div>
+
+                                <div class="row mb-3">
+                                    <div class="col-12 col-md-6" >
+                                        <div class="input-group">
+                                            <input class="form-control" maxlength="10" type="text" oninput="this.value=this.value.replace(/[^0-9]/g,'');" placeholder="Phone Number" id="phone{{$i}}" name="phone[]">
+                                        </div> 
+                                        <div id="phoneerror{{$i}}"></div>  
+                                    </div>
+
+                                    <div class="col-12 col-md-6">
+                                        <div class="input-group">
+                                            <input type="text" id="additonalemail{{$i}}" name="email[]" class="form-control" placeholder="Email">
+                                        </div>
+                                        <div id="emailerror{{$i}}"></div>
+                                    </div>
+                                </div>
+                                @endfor
+
+                                <div id="emailerror{{$i}}" data-index="{{$i}}"></div>
+
+                                  <div class="modal-footer">
+                                        <button type="button" id="close-modal" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                        <button type="button" id="saveadditional" class="btn btn-primary saveadditional">Submit</button>
+                                    </div>
+                                </form>
+
+                                @else
+                                <div class="row">
+                                    <h4>You Can not Create a Additional User</h4>
+                                </div>
+                                 @endif
+                            </div>
+
+                        </div>
+                    </div>
+              </div>
+            </div>
+        </div>
+    </div>
+</div>
+<meta name="csrf-token" content="{{ csrf_token() }}" />
 @endsection
 @section('javascript')
 <script>
     $(document).ready(function() {
-        $(document).on('click', '.delete', function() {
-            var href = $(this).data('href');
-            swal({
-                title: "",
-                text: "{{__('Are you sure? Delete this Currency!')}}",
-                type: "warning",
-                showCancelButton: true,
-                confirmButtonClass: "btn-info",
-                confirmButtonText: "{{__('Yes, delete it!')}}",
-                cancelButtonText: "{{__('Cancel')}}",
-                closeOnConfirm: true
-            },
-            function(){
-                location.href = href;
-            });
+        $("#success-alert").fadeTo(2000, 500).slideUp(500, function(){
+            $("#success-alert").slideUp(500);
+        });
+        $("#danger-alert").fadeTo(2000, 500).slideUp(500, function(){
+            $("#danger-alert").slideUp(500);
         });
     });
+
+    $(document).on('click','.freetoproacess',function()
+    {
+        $('#freetopromodel').modal('show');
+    });
+
+    $(document).on('click','.additionaluser',function()
+    {
+        $('#additionalusermodel').modal('show');
+    });
+
+
+   $(document).on('click','#save_freetopro',function(){
+   	alert("test");
+    var formdata=$('#freetoproform').serialize();
+      $.ajax(
+        {
+
+          url:"{{route('registration-update')}}",
+          type: "post",
+          data: formdata,
+          dataType:'JSON',
+          success: function(data)
+          {
+
+           if (data.status == 1) 
+            {
+               window.location.href = "{{ route('checkout') }}";
+               // setTimeout(function() {
+               //     location.reload();
+               // }, 4000);
+            }
+            
+            if (data.status == 0) {
+                $('#nameerrorshow, #jobtitleerrorshow, #emailerrorshow, #phoneerrorshow').hide();
+                if(data.errors)
+                {
+                    if(data.errors.name){$('#nameerror').html('<strong id="nameerrorshow" style="color:red">'+ data.errors.name + '</strong>');}
+                    if (data.errors.email) {$('#emailerror').html('<strong id="emailerrorshow" style="color:red">' + data.errors.email + '</strong>');}
+                    if(data.errors.phone){$('#phoneerror').html('<strong id="phoneerrorshow" style="color:red">'+ data.errors.phone +'</strong>');}
+                }
+                if(data.errormsg)
+                {
+                    $('#errormsg').html('<strong id="errormsg" style="color:red">Something went Wrong Please Try again.</strong>');
+                }
+            }
+
+          }
+        });
+  });
+
+
+
+
+   $(document).on('click','#saveadditional',function(){
+    var formdata=$('#additionaluserform').serialize();
+      $.ajax(
+        {
+          url:"{{route('storeadditionaluser')}}",
+          type: "post",
+          data: formdata,
+          dataType:'JSON',
+          success: function(data)
+          {
+
+            if (data.status == 1) 
+            {
+                window.location.href = "{{ route('additionalcheckout') }}";
+            }
+            
+            if (data.status == 0 && data.errors) {
+
+                $.each(data.errors, function(key, value) {
+                    if (key.includes('email')) {
+                        var index = key.replace('email', '').replace('.', '');
+                        $('#emailerror' + index).html('<strong style="color:red">' + value[0] + '</strong>');
+                    }
+
+                    if (key.includes('phone')) {
+                        var index = key.replace('phone', '').replace('.', '');
+                        $('#phoneerror' + index).html('<strong style="color:red">' + value[0] + '</strong>');
+                    }
+
+                    if (key.includes('name')) {
+                        var index = key.replace('name', '').replace('.', '');
+                        $('#nameerror' + index).html('<strong style="color:red">' + value[0] + '</strong>');
+                    }
+
+                    if (key.includes('job_title')) {
+                        var index = key.replace('job_title', '').replace('.', '');
+                        $('#jobtitleerror' + index).html('<strong style="color:red">' + value[0] + '</strong>');
+                    }
+                });
+            }
+
+          }
+        });
+  });
 </script>
 @endsection
